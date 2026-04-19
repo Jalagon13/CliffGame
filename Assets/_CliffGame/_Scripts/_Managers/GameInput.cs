5d1dad311b2bd3485265116a5cb23da2d27b61bb
Jalagon <A1ag0nJus1in@gmail.com>
2026-04-20 06:56:44 +0900

    Building system removed

--- Assets/_CliffGame/_Scripts/_Managers/GameInput.cs
+++ Assets/_CliffGame/_Scripts/_Managers/GameInput.cs
@@ -11,10 +11,6 @@ namespace CliffGame
         public event Action<Vector2> OnMove;
         public event Action OnJump;
         public event Action<Vector2> OnLook;
-        public event Action OnToggleBuildMode;
-        public event Action<BuildPieceType> OnBuildPieceSelected;
-        public event Action OnRotateBuild;
-        public event Action OnPlaceBuild;
 
         private PlayerInput _playerInput;
 
@@ -23,10 +19,6 @@ namespace CliffGame
         public bool JumpPressed { get; private set; }
         public bool IsHoldingDownJump { get; private set; }
         public Vector2 LookInput { get; private set; }
-        public bool ToggleBuildModePressed { get; private set; }
-        public bool RotateBuildPressed { get; private set; }
-        public bool PlaceBuildPressed { get; private set; }
-        public BuildPieceType? PendingBuildPieceSelection { get; private set; }
 
         private void Awake()
         {
@@ -49,13 +41,6 @@ namespace CliffGame
             
             _playerInput.Player.Jump.started += GameInput_OnJump;
             _playerInput.Player.Jump.canceled += GameInput_OnJump;
-            
-            _playerInput.Build.ToggleBuildMode.started += BuildAction_OnToggleBuildMode;
-            _playerInput.Build.RotateBuild.started += BuildAction_OnRotateBuild;
-            _playerInput.Build.PlaceBuild.started += BuildAction_OnPlaceBuild;
-            _playerInput.Build.SelectFloor.started += BuildAction_OnSelectFloor;
-            _playerInput.Build.SelectWall.started += BuildAction_OnSelectWall;
-            _playerInput.Build.SelectRamp.started += BuildAction_OnSelectRamp;
         }
         
         private void OnDestroy()
@@ -76,13 +61,6 @@ namespace CliffGame
             
             _playerInput.Player.Jump.started -= GameInput_OnJump;
             _playerInput.Player.Jump.canceled -= GameInput_OnJump;
-
-            _playerInput.Build.ToggleBuildMode.started -= BuildAction_OnToggleBuildMode;
-            _playerInput.Build.RotateBuild.started -= BuildAction_OnRotateBuild;
-            _playerInput.Build.PlaceBuild.started -= BuildAction_OnPlaceBuild;
-            _playerInput.Build.SelectFloor.started -= BuildAction_OnSelectFloor;
-            _playerInput.Build.SelectWall.started -= BuildAction_OnSelectWall;
-            _playerInput.Build.SelectRamp.started -= BuildAction_OnSelectRamp;
         }
 
         private void PlayerInput_OnLook(InputAction.CallbackContext context)
@@ -114,43 +92,6 @@ namespace CliffGame
             OnMove?.Invoke(MoveInput);
         }
 
-        private void BuildAction_OnToggleBuildMode(InputAction.CallbackContext _)
-        {
-            ToggleBuildModePressed = true;
-            OnToggleBuildMode?.Invoke();
-        }
-
-        private void BuildAction_OnSelectFloor(InputAction.CallbackContext _)
-        {
-            PendingBuildPieceSelection = BuildPieceType.Floor;
-            OnBuildPieceSelected?.Invoke(BuildPieceType.Floor);
-        }
-
-        private void BuildAction_OnSelectWall(InputAction.CallbackContext _)
-        {
-            PendingBuildPieceSelection = BuildPieceType.Wall;
-            OnBuildPieceSelected?.Invoke(BuildPieceType.Wall);
-        }
-
-        private void BuildAction_OnSelectRamp(InputAction.CallbackContext _)
-        {
-            PendingBuildPieceSelection = BuildPieceType.Ramp;
-            OnBuildPieceSelected?.Invoke(BuildPieceType.Ramp);
-        }
-
-        private void BuildAction_OnRotateBuild(InputAction.CallbackContext _)
-        {
-            RotateBuildPressed = true;
-            OnRotateBuild?.Invoke();
-        }
-
-        private void BuildAction_OnPlaceBuild(InputAction.CallbackContext _)
-        {
-            PlaceBuildPressed = true;
-            OnPlaceBuild?.Invoke();
-        }
-
-
         public bool ConsumeJumpPressed()
         {
             if (!JumpPressed)
@@ -160,60 +101,6 @@ namespace CliffGame
 
             JumpPressed = false;
             return true;
-        }
-
-        public bool ConsumeToggleBuildModePressed()
-        {
-            if (!ToggleBuildModePressed)
-            {
-                return false;
-            }
-
-            ToggleBuildModePressed = false;
-            return true;
-        }
-
-        public bool ConsumeRotateBuildPressed()
-        {
-            if (!RotateBuildPressed)
-            {
-                return false;
-            }
-
-            RotateBuildPressed = false;
-            return true;
-        }
-
-        public bool ConsumePlaceBuildPressed()
-        {
-            if (!PlaceBuildPressed)
-            {
-                return false;
-            }
-
-            PlaceBuildPressed = false;
-            return true;
-        }
-
-        public bool TryConsumeBuildPieceSelection(out BuildPieceType selectedPiece)
-        {
-            if (!PendingBuildPieceSelection.HasValue)
-            {
-                selectedPiece = default;
-                return false;
-            }
-
-            selectedPiece = PendingBuildPieceSelection.Value;
-            PendingBuildPieceSelection = null;
-            return true;
-        }
-
-        public void ClearBuildCommandBuffer()
-        {
-            ToggleBuildModePressed = false;
-            RotateBuildPressed = false;
-            PlaceBuildPressed = false;
-            PendingBuildPieceSelection = null;
         }
     }
 }
